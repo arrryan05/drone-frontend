67806f8f103d6aef2897f62d9346916a0c796dc9
aryan7424 <aryan@branddy.net>
2025-07-25 20:15:39 +0530

    initial commit

---
+++ src/types/dtos.ts
@@ -0,0 +1,8 @@
+// Shared data transfer objects (empty for now)
+export interface MissionDTO {
+  /* TODO: id, waypoints, params */
+}
+
+export interface DroneDTO {
+  /* TODO: id, name, status, batteryPct */
+}
